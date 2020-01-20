879c45120d255099654bb8c1eb41df3657002100
Mintcz1412 <mintcz80@gmail.com>
2020-01-20 22:41:37 +0700

    Final project

--- res/screens/HomeScreen.js
+++ res/screens/HomeScreen.js
@@ -2,70 +2,93 @@ import React from 'react';
 import { Text, View, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
 //import Header from '../components/header';
 import { Button, Icon, Header, Input  } from 'react-native-elements';
+import { array } from 'prop-types';
 
+const increment = 0;
+const nthTerm_tem = "";
 
 export default class HomeScreen extends React.Component {
- 
+  
+
   static navigationOptions = {
     header: null,
   };
   state = {
-    ValueCal: ''
+    ValueCal1: '',
+    ValueCal2: '',
+    ValueCal3: '',
+    ValueCal4: '',
+    nthTerm: '',
+    listNumber: [],
   }
 
-  nextHole(array,until) {
-    this.array=array;
-    this.lastIndex=0;
-    this.lastItem=0;
-    this.until=until;
-    this.loopFinished=false;
-    this.done=false;
-    this.reset = function(){
-        this.lastIndex=0;
-        this.lastItem=0;
-        this.done=false;
-        this.loopFinished=false;
-    }
-
-    this.getNext = function(){
-
-      if (this.done){
-          return null;
-      }
-
-      if (!this.loopFinished){                
-          for(loop=this.lastIndex;loop<this.array.length ;loop++){
-              if (this.array[loop]!=this.lastItem+1){
-
-                  this.lastItem++;
-                  this.lastIndex=loop;
-                  if (this.lastItem>until)
-                      break;
-
-
-                  return this.lastItem;
-              }else{
-                  this.lastItem=this.array[loop];
-              }
-
-          }
-
-      }
-      this.loopFinished=true;
-      if (this.lastItem < this.until){
-          return (++this.lastItem);
-      }
-      this.done=true;
-      return null;
-    }
-
+  hasSeriesRelation =(arr, relationFn) =>{
+    if (arr.length < 3) return false;
+    return arr.every(relationFn);
+  }
+  isArithmeticAtIndex =(elm, i, arr) =>{
+    if (i < 2) return true;
+    return arr[i] - arr[i-1] === arr[i-1] - arr[i-2];
+  }
+  isGeometricAtIndex =(elm, i, arr) =>{
+    if (i < 2) return true;
+    if (arr[i-1] == 0 || arr[i-2] == 0) return false;
+    return arr[i] / arr[i-1] === arr[i-1] / arr[i-2];
+  }
+  isFibonacciAtIndex =(elm, i, arr) =>{
+    if (i < 2) return true;
+    return arr[i] === arr[i-1] + arr[i-2];
   }
 
-  
 
   onCoinMoreDetailSelected =(text) =>{
-    if(this.state.ValueCal != "" && this.state.ValueCal != undefined){
 
+    
+
+    if((this.state.ValueCal1 != "" && this.state.ValueCal1 != undefined) &&
+    (this.state.ValueCal2 != "" && this.state.ValueCal2 != undefined) &&
+    (this.state.ValueCal3 != "" && this.state.ValueCal3 != undefined) &&
+    (this.state.ValueCal4 != "" && this.state.ValueCal4 != undefined)){
+
+      this.setState({ listNumber: [] });
+      this.setState({ nthTerm: "" });
+      this.increment = 0;
+      this.nthTerm_tem = "";
+      
+      this.state.listNumber.push(this.state.ValueCal1*1);
+      this.state.listNumber.push(this.state.ValueCal2*1);
+      this.state.listNumber.push(this.state.ValueCal3*1);
+      this.state.listNumber.push(this.state.ValueCal4*1);
+      console.log(this.state.listNumber);
+      
+      const fin1 = this.hasSeriesRelation(this.state.listNumber, this.isArithmeticAtIndex);
+      const fin2 = this.hasSeriesRelation(this.state.listNumber, this.isGeometricAtIndex);
+      const fin3 = this.hasSeriesRelation(this.state.listNumber, this.isFibonacciAtIndex);
+      if(fin1){
+       this.increment = this.state.listNumber[1] - this.state.listNumber[0];
+       for(var i = 0; i < 5; i++){
+        this.nthTerm_tem = this.nthTerm_tem+( this.increment * i + this.state.listNumber[0] )+" ";
+       }
+      }else if(fin2){
+      
+      this.increment = this.state.listNumber[1] / this.state.listNumber[0];
+        for(var i = 0; i < 5; i++){
+          this.nthTerm_tem = this.nthTerm_tem+( this.state.listNumber[0] * (Math.pow(this.increment, i)) )+" ";
+        }
+      }else if(fin3){
+        for(var i = 0; i < 5; i++){
+          if(i < 2){
+            this.nthTerm_tem = this.nthTerm_tem+( this.state.listNumber[i] )+" ";
+          }else{
+            this.nthTerm_tem = this.nthTerm_tem+( this.state.listNumber[i-2] + this.state.listNumber[i-1] )+" ";
+          }
+        }
+      }
+
+      this.setState({ nthTerm: this.nthTerm_tem });
+      console.log(this.state.nthTerm);
+      
+     
     }else{
       alert("error please fill all fields!");
     }
@@ -73,24 +96,69 @@ export default class HomeScreen extends React.Component {
   }
   
   render() {
+    
     return (
       <SafeAreaView >
         <Header
           centerComponent={{ text: 'Home', style: { color: '#fff', fontSize: 20, fontWeight: 'bold' } }}
         />
-        <View>
-         <Input name="ValueCal"
-            onChangeText={(text) => this.setState({ValueCal:text})}
-            placeholder='value'
+
+      <View style={{flex: 1, flexDirection: 'row', alignSelf: 'center', justifyContent: 'space-between', marginTop: 50}}>
+       <View style={{width: 50, height: 50}}>
+         <Input name="ValueCal1"
+            onChangeText={(text) => this.setState({ValueCal1:text})}
+            placeholder='0'
           />
         </View>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{width: 50, height: 50}}>
+         <Input name="ValueCal2"
+            onChangeText={(text) => this.setState({ValueCal2:text})}
+            placeholder='0'
+          />
+        </View>
+        <View style={{width: 50, height: 50}}>
+         <Input name="ValueCal3"
+            onChangeText={(text) => this.setState({ValueCal3:text})}
+            placeholder='0'
+          />
+        </View>
+        <View style={{width: 50, height: 50}}>
+         <Input name="ValueCal4"
+            onChangeText={(text) => this.setState({ValueCal4:text})}
+            placeholder='0'
+          />
+        </View>
+      </View>
+        
 
+      
+        
+        <View style={{alignItems: 'center' }}>
+      
+         <Text style={{fontSize:25, marginTop: 70, color: '#999999'}}>{this.state.nthTerm}</Text>
+       
           <TouchableOpacity
             style={styles.button}
             onPress={this.onCoinMoreDetailSelected.bind(this,1)}>
             <Text style={{ color: "#ffffff" }}>Calculate</Text>
           </TouchableOpacity>
+        </View>
+
+        <View>
+         <Text style={{fontSize:15, marginTop: 10, marginLeft: 8}}>
+           *หมายเหตุ  {"\n"}
+            สามารถใช้ชุดตัวเลขคำนวณโดยใช้สูตร {"\n"}
+            - Arithmetic Sequences  {"\n"}
+            - Geometric Sequence  {"\n"}
+            - Fibonacci Numbers Sequence  {"\n"}
+            ตัวอย่างชุดตัวเลข {"\n"}
+            (2,5,8,11) {"\n"}
+            (3,9,27,81) {"\n"}
+            (1,1,2,3)  {"\n\n"} 
+            ยังไม่สามารถใช้สูตรได้ {"\n"}
+            - Square Number Sequence {"\n"}
+            - Incremental Sequence {"\n"}
+           </Text>
         </View>
       </SafeAreaView>
     );
@@ -105,6 +173,6 @@ const styles = StyleSheet.create({
     width: 300,
     height: 50,
     borderRadius: 10,
-    marginTop:100,
+    marginTop:20,
   },
 });
--- res/screens/SearchScreen.js
+++ res/screens/SearchScreen.js
@@ -1,32 +1,313 @@
-import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
-//import Header from '../components/header';
-import { Button, Icon, Header } from 'react-native-elements';
+import React, { Component } from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  Alert,
+  Image,
+  StyleSheet,
+} from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+//import GPSState from 'react-native-gps-state';
+import MapView, { Marker } from 'react-native-maps';
+import { Button, Icon, Header, Input  } from 'react-native-elements';
+
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 13.806212;
+const LONGITUDE = 100.537707;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const currentLocationImage = require('../assets/currentLocation.png');
 
 export default class SearchScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
-  render() {
-    return (
-      <SafeAreaView>
-        <Header
-          centerComponent={{ text: 'Place search', style: { color: '#fff', fontSize: 20, fontWeight: 'bold' } }}
-        />
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text style={{ marginTop: 50, fontSize: 25 }}>Search!</Text>
-        </View>
-      </SafeAreaView>
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      goSearch: false,
+      indicator: false,
+    };
+  }
+
+  componentDidMount = () => {
+    //this.reqPermission();
+  }
+
+  componentWillUnmount = () => {
+    //navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  findMe() {
+    this.setState({ indicator: true });
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        const { latitude, longitude } = coords;
+        console.log(coords);
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.001,
+          },
+        });
+        this.setState({ indicator: false });
+      },
+      (error) => {
+        console.log('find me :', error);
+        if (error.code === 2) {
+          Alert.alert(
+            'No location provider available',
+            'Please activate your GPS',
+            [
+              { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+              { text: 'OK', onPress: () => GPSState.openSettings() },
+            ],
+            { cancelable: false },
+          );
+        }
+        this.setState({ indicator: false });
+      },
+      { enableHighAccuracy: false, timeout: 20000, maximumAge: 10000 }
     );
   }
+
+  renderSearch() {
+    const { goSearch, region } = this.state;
+    return (
+      <GooglePlacesAutocomplete
+        placeholder='ค้นหาสถานที่'
+        minLength={2}
+        autoFocus={goSearch}
+        returnKeyType='default'
+        fetchDetails
+        onPress={(data, details = null) => {
+          console.log('onPress: ', data, details);
+          this.setState({
+            region: {
+              latitude: details.geometry.location.lat,
+              longitude: details.geometry.location.lng,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            },
+            goSearch: false,
+          });
+        }}
+        query={{
+          key: 'AIzaSyDFM5h6YuTR4LagwOu66hdtkBBAZ2RfzoQ',
+          language: 'th',
+        }}
+        styles={{
+          textInputContainer: {
+            backgroundColor: 'white',
+            // marginHorizontal: 20,
+            borderRadius: 12,
+            height: 45,
+            width: '100%',
+          },
+          textInput: {
+            marginLeft: 0,
+            marginRight: 0,
+            height: 40,
+            color: '#5d5d5d',
+            fontSize: 16,
+          },
+          listView: {
+            backgroundColor: 'white',
+            width: '100%',
+            height: goSearch ? height * 0.25 : 0,
+            // marginHorizontal: 20,
+            borderBottomRightRadius: 12,
+            borderBottomLeftRadius: 12,
+          },
+        }}
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={200}
+      />
+    );
+  }
+
+  render() {
+    const { goSearch, indicator, region } = this.state;
+    console.log('region :',region);
+    
+    const hitSlop = {
+      top: 15,
+      bottom: 15,
+      left: 15,
+      right: 15,
+    };
+    return (
+      <View style={{ flex: 1 }}>
+        <Header
+          centerComponent={{ text: 'Map', style: { color: '#fff', fontSize: 20, fontWeight: 'bold' } }}
+        />
+        <MapView
+          style={styles.container}
+          showsUserLocation
+          showsMyLocationButton={false}
+          region={region}
+          onPress={(e) => {
+            console.log('map press :', e.nativeEvent);
+            this.setState({
+              region: {
+                latitude: e.nativeEvent.coordinate.latitude,
+                longitude: e.nativeEvent.coordinate.longitude,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+              },
+              goSearch: false,
+            });
+          }
+          }
+        >
+          <Marker
+            draggable
+            coordinate={region}
+            onDragEnd={(e) => {
+              this.setState({
+                goSearch: false,
+                region: {
+                  latitude: e.nativeEvent.coordinate.latitude,
+                  longitude: e.nativeEvent.coordinate.longitude,
+                  latitudeDelta: LATITUDE_DELTA,
+                  longitudeDelta: LONGITUDE_DELTA,
+                },
+              });
+            }}
+          />
+
+        </MapView>
+        <View style={styles.searchLocationContainer}>
+          {goSearch ? this.renderSearch() : (
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({ goSearch: true });
+              }}
+              style={styles.searchLocationButton}
+            >
+              <Text style={styles.searchLocationText}>
+                ค้นหาสถานที่
+              </Text>
+            </TouchableOpacity>)}
+        </View>
+
+     
+        <View style={styles.completeView}>
+          <View style={{ width: '50%', height: '100%'}}>
+            <Text style={styles.completeText}>
+              {region.latitude}
+            </Text>
+          </View>
+          <View style={{ width: '50%', height: '100%', borderLeftWidth: 1, borderColor: 'grey'}}>
+            <Text style={styles.completeText}>
+              {region.longitude}
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
 }
+
+
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    width: 300,
-    marginTop: 16,
+  container: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    zIndex: 1 ,
   },
+  completeSection: {
+    width,
+    borderTopWidth: 0.5,
+    borderColor: "#e4e4e4",
+    backgroundColor: 'transparent'
+  },
+  findMeView: {
+    position: 'absolute',
+    right: 20,
+    bottom: 100,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    zIndex: 5,
+  },
+  mapButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 85 / 2,
+    backgroundColor: 'rgba(252, 253, 253, 0.9)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowRadius: 8,
+    shadowOpacity: 0.12,
+    opacity: 0.9,
+    zIndex: 5,
+  },
+  searchLocationContainer: {
+    width: '90%',
+    alignSelf: 'center',
+    position: 'absolute',
+    top: 100,
+    zIndex: 5,
+    backgroundColor: 'transparent',
+  },
+  searchLocationButton: {
+    backgroundColor: 'white',
+    height: 45,
+    // marginHorizontal: 20,
+    borderRadius: 12,
+    justifyContent: 'center',
+  },
+  searchLocationText: {
+    fontSize: 18,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    lineHeight: 30,
+    letterSpacing: 0,
+    textAlign: 'right',
+    padding: 20,
+  },
+  completeView: {
+    height: 45,
+    width: width * 0.9,
+    marginHorizontal: 20,
+    marginVertical: 20,
+    padding: 6,
+    borderRadius: 12,
+    position: 'absolute',
+    bottom: 20,
+    zIndex: 5,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+
+  },
+  completeText: {
+    fontSize: 18,
+    fontWeight: "600",
+    fontStyle: "normal",
+    lineHeight: 30,
+    letterSpacing: 0,
+    textAlign: "center",
+    color: 'grey'
+  },
+  currentLocationImage: {
+    width: 30,
+    height: 30,
+  }
 });
